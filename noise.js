7d8d70645a9855e0e98b5a898a2d7f05fff40998
qqdipps <sdipp007@gmail.com>
2019-05-28 16:21:27 -0700

    basic notes playing

--- noise.js
+++ noise.js
@@ -1,3 +1,28 @@
-$(document).ready( function() {
+$(document).ready(function() {
   // your code here
+  const tones = [
+    { note: '.c', audio: '#cAudio', key: 'c' },
+    { note: '.d', audio: '#dAudio', key: 'd' },
+    { note: '.e', audio: '#eAudio', key: 'e' },
+    { note: '.f', audio: '#fAudio', key: 'f' },
+    { note: '.g', audio: '#gAudio', key: 'g' },
+    { note: '.a', audio: '#aAudio', key: 'a' },
+    { note: '.b', audio: '#bAudio', key: 'b' },
+  ];
+
+  tones.forEach(tone => {
+    const note = $(`${tone.note}`);
+    const audio = $(`${tone.audio}`);
+    note.click(() => {
+      console.log('click');
+      audio[0].load();
+      audio[0].play();
+    });
+    $('body').keydown(event => {
+      if (event.key === tone.key) {
+        audio[0].load();
+        audio[0].play();
+      }
+    });
+  });
 });
